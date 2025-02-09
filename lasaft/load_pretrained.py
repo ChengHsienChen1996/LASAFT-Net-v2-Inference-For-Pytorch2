16a668c87db2ede24274aec9462e876be96dea60
ChengHsienChen1996 <g8963458741@gmail.com>
2025-02-09 19:41:02 +0800

    feat: 新增gpu選項

--- lasaft/load_pretrained.py
+++ lasaft/load_pretrained.py
@@ -6,16 +6,16 @@ from pkg_resources import resource_filename
 from safetensors.torch import load_file
 
 
-def get_v2_large_709():
+def get_v2_large_709(use_gpu: bool=False):
     conf_path = resource_filename('lasaft', 'pretrained/v2_large/')
     conf_path = Path(conf_path)
     ckpt_path = conf_path.joinpath('lasaft_v2_large_709.safetensors')
-
+    device = "cuda" if use_gpu else "cpu"
     with open(conf_path.joinpath('config.yaml')) as f:
         train_config = OmegaConf.load(f)
         model_config = train_config['model']
 
-        model = hydra.utils.instantiate(model_config).to('cpu')
+        model = hydra.utils.instantiate(model_config).to(device)
 
         try:
             loaded_state_dict = load_file(str(ckpt_path))
@@ -28,16 +28,16 @@ def get_v2_large_709():
     return model
 
 
-def get_mdx_light_v2_699():
+def get_mdx_light_v2_699(use_gpu: bool=False):
     conf_path = resource_filename('lasaft', 'pretrained/v2_light/')
     conf_path = Path(conf_path)
     ckpt_path = conf_path.joinpath('lasaft_v2_light_669_for_mdx.safetensors')
-
+    device = "cuda" if use_gpu else "cpu"
     with open(conf_path.joinpath('config.yaml')) as f:
         train_config = OmegaConf.load(f)
         model_config = train_config['model']
 
-        model = hydra.utils.instantiate(model_config).to('cpu')
+        model = hydra.utils.instantiate(model_config).to(device)
 
         try:
             loaded_state_dict = load_file(str(ckpt_path))
